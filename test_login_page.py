a5a50d3a7e3d070a2f25d36a67b130cd5abfa459
zzaakiirr <zzaakiirr@gmail.com>
2018-12-09 10:27:19 +0300

    Splited all tests to different classes, GenericTests class ready

--- test_login_page.py
+++ test_login_page.py
@@ -3,15 +3,34 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
 
-class GoogleSearchTests(unittest.TestCase):
-    """  """
+# def is_element_exist_in_page(driver, element_xpath):
+#     try:
+#         driver.find_element_by_xpath(element_xpath)
+#     except NoSuchElementException:
+#         return False
+#     return True
+
+
+class MainPageTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Firefox()
         cls.driver.maximize_window()
         cls.start_url = "http://www.google.com"
         cls.driver.get(cls.start_url)
-        cls.driver.implicitly_wait(10)
+        # cls.driver.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+
+
+class GenericTests(MainPageTestCase):
+    """  """
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
 
     def setUp(self):
         ...
@@ -22,26 +41,69 @@ class GoogleSearchTests(unittest.TestCase):
 
     def test_is_search_input_field_exist(self):
         try:
-            search_input_field = self.driver.find_element_by_xpath(
-                "//div/div/form/div/div/div/div/div/input[@name='q']"
-            )
+            search_input_field = self.driver.find_element_by_name("q")
         except NoSuchElementException:
-            self.fail("search input field doesn't exist")
+            self.fail("Search input field doesn't exist")
 
     def test_is_logo_exist(self):
         try:
             logo = self.driver.find_element_by_id("hplogo")
         except NoSuchElementException:
-            self.fail("logo doesn't exist")
+            self.fail("Logo doesn't exist")
 
     def test_is_search_button_exist(self):
         try:
-            search_button = self.driver.find_element_by_xpath(
-                "//div/div/form/div/div/div/center/input[@name='btnI']")
+            search_button = self.driver.find_element_by_name("btnK")
         except NoSuchElementException:
-            self.fail("search button doesn't exist")
+            self.fail("Search button doesn't exist")
 
-    # def test_lucky_button_exist(self):
+    def test_is_i_am_lucky_button_exist(self):
+        try:
+            i_am_lucky_button = self.driver.find_element_by_name("btnI")
+        except NoSuchElementException:
+            self.fail("I am lucky button doesn't exist")
+
+    def test_is_not_voice_search_button_exist(self):
+        with self.assertRaises(NoSuchElementException):
+            self.driver.find_element_by_class_name("voice_search_button")
+
+    def test_page_loads_without_active_screen_loupe(self):
+        with self.assertRaises(NoSuchElementException):
+            self.driver.find_element_by_id("kbd")
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+
+class SearchInputFieldTests():
+    ...
+
+
+class IamFeelingLuckyButtonTests():
+    ...
+
+
+class SearchButtonTests(unittest.TestCase):
+    """  """
+
+    def test_filled_search_input_field_submitting_by_button(self):
+        ...
+
+    def test_filled_search_input_field_submitting_by_enter_key(self):
+        ...
+
+
+class ScreenKeyboardTests(unittest.TestCase):
+    """  """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Firefox()
+        cls.driver.maximize_window()
+        cls.start_url = "http://www.google.com"
+        cls.driver.get(cls.start_url)
+        # cls.driver.implicitly_wait(10)
 
     # def test_screen_loupe_button(self):
 
