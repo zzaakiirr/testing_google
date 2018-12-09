fca0ebe7f1c1ae911067091cac930dfd8b6cffef
zzaakiirr <zzaakiirr@gmail.com>
2018-12-09 14:23:07 +0300

    Added IamLuckyButtonTests, SearchButtonTests, SearchInputFieldTests test classes + edited GenericTests

--- test_login_page.py
+++ test_login_page.py
@@ -1,14 +1,16 @@
 import unittest
+import time
+
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
-
-# def is_element_exist_in_page(driver, element_xpath):
-#     try:
-#         driver.find_element_by_xpath(element_xpath)
-#     except NoSuchElementException:
-#         return False
-#     return True
+import requests
 
 
 class MainPageTestCase(unittest.TestCase):
@@ -16,9 +18,9 @@ class MainPageTestCase(unittest.TestCase):
     def setUpClass(cls):
         cls.driver = webdriver.Firefox()
         cls.driver.maximize_window()
-        cls.start_url = "http://www.google.com"
+        cls.start_url = "https://www.google.com/"
         cls.driver.get(cls.start_url)
-        # cls.driver.implicitly_wait(10)
+        cls.driver.implicitly_wait(10)
 
     @classmethod
     def tearDownClass(cls):
@@ -26,7 +28,7 @@ class MainPageTestCase(unittest.TestCase):
 
 
 class GenericTests(MainPageTestCase):
-    """  """
+    """ Generic tests for checking if page loads with correct elements"""
 
     @classmethod
     def setUpClass(cls):
@@ -34,6 +36,10 @@ class GenericTests(MainPageTestCase):
 
     def setUp(self):
         ...
+
+    def test_url_reaching_return_success_status_code(self):
+        response = requests.get(self.start_url)
+        self.assertEqual(response.status_code, 200)
 
     def test_is_page_title_correct(self):
         expected_title = 'Google'
@@ -63,7 +69,7 @@ class GenericTests(MainPageTestCase):
         except NoSuchElementException:
             self.fail("I am lucky button doesn't exist")
 
-    def test_is_not_voice_search_button_exist(self):
+    def test_does_not_voice_search_button_exist(self):
         with self.assertRaises(NoSuchElementException):
             self.driver.find_element_by_class_name("voice_search_button")
 
@@ -76,61 +82,96 @@ class GenericTests(MainPageTestCase):
         super().tearDownClass()
 
 
-class SearchInputFieldTests():
-    ...
-
-
-class IamFeelingLuckyButtonTests():
-    ...
-
-
-class SearchButtonTests(unittest.TestCase):
-    """  """
-
-    def test_filled_search_input_field_submitting_by_button(self):
-        ...
-
-    def test_filled_search_input_field_submitting_by_enter_key(self):
-        ...
-
-
-class ScreenKeyboardTests(unittest.TestCase):
-    """  """
-
+class SearchInputFieldTests(MainPageTestCase):
     @classmethod
     def setUpClass(cls):
-        cls.driver = webdriver.Firefox()
-        cls.driver.maximize_window()
-        cls.start_url = "http://www.google.com"
-        cls.driver.get(cls.start_url)
-        # cls.driver.implicitly_wait(10)
+        super().setUpClass()
 
-    # def test_screen_loupe_button(self):
+    def setUp(self):
+        self.driver.back()
+        ignored_exceptions = (
+            NoSuchElementException,
+            StaleElementReferenceException,
+        )
+        wait = WebDriverWait(
+            self.driver, 10, ignored_exceptions=ignored_exceptions,
+        )
+        self.search_input_field = wait.until(
+            EC.presence_of_element_located((By.NAME, "q"))
+        )
 
-    # def test_close_screen_loupe_close_button(self):
-        # wait = WebDriverWait(driver, 10)
-        # self.driver.implicitly_wait(10)
-        # self.assertEqual(self.start_url, self.driver.current_url)
-    # def test_search_field_exist(self):
+    def test_empty_search_input_field_can_not_submit_by_return_key(self):
+        self.search_input_field.send_keys(Keys.RETURN)
+        time.sleep(3)
+        self.assertEqual(self.driver.current_url, self.start_url)
 
-    # def test_search_in_python_org(self):
-    #     #Load the main page. In this case the home page of Python.org.
-    #     main_page = page.MainPage(self.driver)
-    #     #Checks if the word "Python" is in title
-    #     assert main_page.is_title_matches(), "python.org title doesn't match."
-    #     #Sets the text of search textbox to "pycon"
-    #     main_page.search_text_element = "pycon"
-    #     main_page.click_go_button()
-    #     search_results_page = page.SearchResultsPage(self.driver)
-    #     #Verifies that the results page is not empty
-    #     assert search_results_page.is_results_found(), "No results found."
+    def test_filled_search_input_field_submit_by_return_key(self):
+        self.search_input_field.send_keys("Kaspersky", Keys.RETURN)
+        time.sleep(3)
+        self.assertNotEqual(self.driver.current_url, self.start_url)
+
+    def test_search_input_field_saves_logged_user_search_history(self):
+        ...
 
     @classmethod
     def tearDownClass(cls):
-        cls.driver.close()
+        super().tearDownClass()
 
-    # def tearDown(self):
-    #     self.driver.close()
+
+class IamLuckyButtonTests(MainPageTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    # def test_i_am_lucky_button_redirect(self):
+    #     i_am_lucky_button = WebDriverWait(self.driver, 10).until(
+    #         EC.element_to_be_clickable((By.NAME, "btnI"))
+    #     )
+    #     i_am_lucky_button.click()
+    #     time.sleep(3)
+    #     expected_url = "https://www.google.com/doodles/"
+    #     self.assertEqual(expected_url, self.driver.current_url)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+
+class SearchButtonTests(MainPageTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.search_button = cls.driver.find_element_by_name("btnK")
+        cls.search_input_field = cls.driver.find_element_by_name("q")
+
+    # Doesn't work self.search_button.click()
+    # def test_filled_search_input_field_submit_by_search_button(self):
+    #     self.search_input_field.send_keys("Kaspersky")
+    #     self.search_button.click()
+    #     time.sleep(3)
+    #     self.assertNotEqual(self.driver.current_url, self.start_url)
+
+    def test_empty_search_input_field_can_not_submit_by_search_button(self):
+        self.search_input_field.send_keys(Keys.RETURN)
+        time.sleep(3)
+        self.assertEqual(self.driver.current_url, self.start_url)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+
+# class ScreenKeyboardTests(MainPageTestCase):
+#     @classmethod
+#     def setUpClass(cls):
+#         super().setUpClass()
+
+#     def test_screen_keyboard_button_inside_search_input_field_opens(self):
+        
+
+#     @classmethod
+#     def tearDownClass(cls):
+#         super().tearDownClass()
 
 if __name__ == "__main__":
     unittest.main()
